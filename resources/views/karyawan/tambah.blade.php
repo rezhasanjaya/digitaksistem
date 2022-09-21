9282db88b131e13f98b1b30b80a7722c1ba2d5db
adimuhamad <adi.10119253@mahasiswa.unikom.ac.id>
2022-09-21 14:03:09 +0700

    update tambah karyawan

--- resources/views/karyawan/tambah.blade.php
+++ resources/views/karyawan/tambah.blade.php
@@ -18,55 +18,75 @@
                     <form action="" method="">
                         <div class="form-group">
                             <label class="form-control-label">NIK</label>
-                            <input class="form-control" type="text">
+                            <input class="form-control" type="text" value="32165498719">
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Nama</label>
-                            <input class="form-control" type="text">
+                            <input class="form-control" type="text" value="Reza Putra Sancakala">
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Jenis Kelamin</label>
-                            <input class="form-control" type="text">
+                            <select class="form-control" id="jenis_kelamin">
+                                <option>Laki-Laki</option>
+                                <option>Perempuan</option>
+                                <option>Non Biner</option>
+                            </select>
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Unit / Department</label>
-                            <input class="form-control" type="text">
+                            <select class="form-control" id="unit_department">
+                                <option>Logistik</option>
+                                <option>Produksi</option>
+                                <option>Keamanan</option>
+                            </select>
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Tanggal Masuk</label>
-                            <input class="form-control" type="text">
+                            <input class="form-control" type="date" id="tanggal_masuk" value="2018-11-23">
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Alamat</label>
-                            <input class="form-control" type="text">
+                            <textarea class="form-control" id="alamat" rows="3"></textarea>
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Tempat Lahir</label>
-                            <input class="form-control" type="text">
+                            <input class="form-control" type="text" id="tempat_lahir" value="UNIKOM">
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Tanggal Lahir</label>
-                            <input class="form-control" type="text">
+                            <input class="form-control" type="date" id="tanggal_lahir" value="1918-11-23">
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Jabatan</label>
-                            <input class="form-control" type="text">
+                            <input class="form-control" type="text" id="jabatan" value="Manajer">
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Agama</label>
-                            <input class="form-control" type="text">
+                            <select class="form-control" id="agama">
+                                <option>Islam</option>
+                                <option>Kristen</option>
+                                <option>Buddha</option>
+                            </select>
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Status Perkawinan</label>
-                            <input class="form-control" type="text">
+                            <select class="form-control" id="status_kawin">
+                                <option>Menikah</option>
+                                <option>Belum Menikah</option>
+                                <option>Cerai</option>
+                            </select>
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Golongan Darah</label>
-                            <input class="form-control" type="text">
+                            <select class="form-control" id="gol_dar">
+                                <option>A</option>
+                                <option>B</option>
+                                <option>C</option>
+                            </select>
                         </div>
                         <div class="form-group">
                             <label class="form-control-label">Nomor Telpon</label>
-                            <input class="form-control" type="text">
+                            <input class="form-control" type="text" id="nomor_telp" value="+6285319105757">
                         </div>
                         <a href="{{ url('department') }}" class="btn bg-gradient-primary btn-sm mt-3 mb-4 float-end" type="button">+&nbsp; Tambah Karyawan</a>
                     </form>
