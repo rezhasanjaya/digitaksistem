d065718680e9991fb626a27d9bce0ce494a84021
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2022-09-26 15:35:42 +0700

    Test

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -17,7 +17,7 @@ class KaryawanController extends Controller
         return view('karyawan.index', ["title" => "Karyawan"], compact('karyawans'));
     }
 
-    public function tambah()
+    public function create()
     {
         return view('karyawan.tambah', [
             "title" => "Tambah Karyawan",
@@ -25,11 +25,53 @@ class KaryawanController extends Controller
         ]);
     }
 
-    public function store(Karyawan $karyawan,  KaryawanStoreReq $request)
+    public function edit(Karyawan $karyawan)
     {
-        $karyawan->create(array_merge($request->validated()));
+        Department::first()->kode;
+        return view('karyawan.edit', compact('karyawan'), ["title" => "Edit Data Karyawan"]);
+    }
+
+    public function show(Karyawan $karyawan)
+    {
+        return view('karyawan.show', compact('karyawans'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nik' => 'required|unique:karyawan',
+            'nama' => 'required',
+            'jenis_kelamin' => 'required',
+            'unit' => 'required',
+            'tanggal_masuk' => 'required',
+            'alamat' => 'required',
+            'tempat_lahir' => 'required',
+            'tanggal_lahir' => 'required',
+            'jabatan' => 'required',
+            'agama' => 'required',
+            'status_kawin' => 'required',
+            'goldar' => 'required',
+            'telp' => 'required',
+        ]);
+        $karyawan = new Karyawan;
+        $karyawan->nik = $request->nik;
+        $karyawan->nama = $request->nama;
+        $karyawan->jenis_kelamin = $request->jenis_kelamin;
+        $karyawan->unit = $request->unit;
+        $karyawan->tanggal_masuk = $request->tanggal_masuk;
+        $karyawan->alamat = $request->alamat;
+        $karyawan->tempat_lahir = $request->tempat_lahir;
+        $karyawan->tanggal_lahir = $request->tanggal_lahir;
+        $karyawan->jabatan = $request->jabatan;
+        $karyawan->agama = $request->agama;
+        $karyawan->status_kawin = $request->status_kawin;
+        $karyawan->goldar = $request->goldar;
+        $karyawan->telp = $request->telp;
+        $karyawan->save();
+
+        // $department->create(array_merge($request->validated()));
         return redirect()->route('karyawan.index')
-            ->with('success', 'Karyawan Baru Telah Ditambahkan.');
+            ->with('success', 'Karyawan Baru Berhasil Ditambahkan.');
     }
 
     public function destroy(Karyawan $karyawan)
